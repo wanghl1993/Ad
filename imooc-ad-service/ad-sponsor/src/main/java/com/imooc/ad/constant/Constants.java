60285fad814a2aa93f83ed4bc504f50e6bff6042
wanghl <study_yz@163.com>
2020-06-05 17:58:17 +0800

    5-14 course

--- imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/constant/Constants.java
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/constant/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
         public static final String REQUEST_PARAM_ERROR = "请求参数错误";
         public static final String SAME_NAME_ERROR = "存在同名用户";
 
+        public static final String CAN_NOT_FIND_RECORD = "找不到记录";
+        public static final String SAME_NAME_PLAN_ERROR = "存在同名的推广计划";
 
     }
 
