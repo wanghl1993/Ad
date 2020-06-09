f93597d9c2009d8d005cb1b9efe7cd0f8a2e91e0
wanghl <study_yz@163.com>
2020-06-09 17:53:33 +0800

    8-4 start import data(1)

---
+++ imooc-ad-service/ad-sponsor/src/test/java/com/imooc/ad/service/DumpDataService.java
@@ -0,0 +1,36 @@
+package com.imooc.ad.service;
+
+import com.imooc.ad.Application;
+import com.imooc.ad.dao.AdPlanRepository;
+import com.imooc.ad.dao.AdUnitRepository;
+import com.imooc.ad.dao.CreativeRepository;
+import com.imooc.ad.dao.unit_condition.AdUnitDistrictRepository;
+import com.imooc.ad.dao.unit_condition.AdUnitItRepository;
+import com.imooc.ad.dao.unit_condition.AdUnitKeywordRepository;
+import com.imooc.ad.dao.unit_condition.CreativeUnitRepository;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class},
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class DumpDataService {
+
+    @Autowired
+    private AdPlanRepository planRepository;
+    @Autowired
+    private AdUnitRepository unitRepository;
+    @Autowired
+    private CreativeRepository creativeRepository;
+    @Autowired
+    private CreativeUnitRepository creativeUnitRepository;
+    @Autowired
+    private AdUnitDistrictRepository districtRepository;
+    @Autowired
+    private AdUnitItRepository itRepository;
+    @Autowired
+    private AdUnitKeywordRepository keywordRepository;
+
+}
