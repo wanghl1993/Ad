567f2c0e3d09c89680cbcb7779226077caf65c84
wanghl <study_yz@163.com>
2020-06-09 15:13:12 +0800

    so sick 7-9 -> more to more Index

---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/index/adunit/AdUnitObject.java
@@ -0,0 +1,35 @@
+package com.imooc.ad.index.adunit;
+
+import com.imooc.ad.index.adplan.AdPlanObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdUnitObject {
+
+    private Long unitId;
+    private Integer unitStatus;
+    private Integer positionType;
+    private Long planId;
+    private AdPlanObject adPlanObject;
+
+    void update(AdUnitObject newObject){
+
+        if(null != newObject.getUnitId()){
+            this.unitId = newObject.getUnitId();
+        }
+        if(null != newObject.getUnitStatus()){
+            this.unitStatus = newObject.getUnitStatus();
+        }
+        if(null != newObject.getPlanId()){
+            this.planId = newObject.getPlanId();
+        }
+        if(null != newObject.getAdPlanObject()){
+            this.adPlanObject = newObject.getAdPlanObject();
+        }
+    }
+
+}
